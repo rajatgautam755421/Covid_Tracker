e12ff84633f305872d5383d921b0de37454cb096
rajat755421 <rajatgautam832@gmail.com>
2021-05-22 19:52:32 +0545

    5th commit

--- src/myComponents/chart.js
+++ src/myComponents/chart.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Bar, Line } from "react-chartjs-2";
-import { FetchApi } from "./fetchApi";
 
 const Chart = () => {
   const [value, setValue] = useState([]);
